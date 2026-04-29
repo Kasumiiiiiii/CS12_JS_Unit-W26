099717c7d0df85fb5756045e58517e795975701e
Kasumiiiiiii <helenaevapayton@gmail.com>
2026-04-29 10:08:36 -0300

    final check left

--- Homework/JS Project - Guessing Game/script.js
+++ Homework/JS Project - Guessing Game/script.js
@@ -13,6 +13,7 @@ let msgObj = document.querySelector(".message");
 let scoreObj = document.querySelector(".score");
 let highscoreObj = document.querySelector(".highscore");
 let numberObj = document.querySelector(".number");
+let checkBtn = document.querySelector(".check");
 
 // Helper Function
 function getRandomIntInclusive(min, max) {
@@ -38,13 +39,15 @@ function guess() {
     let userGuess = Number(guessObj.value);
     if (userGuess == randNum) {
         msgObj.textContent = "You Win!";
-        document.body.style.backgroundColor = "green";
+        document.body.style.backgroundColor = "#39c039";
         numberObj.textContent = randNum;
+        checkBtn.disabled = true;
+
         // check if score beats previous high score
         if (score > highscore) {
-            highscore = score;
-            highscoreObj.textContent = highscore;
-        }
+          highscore = score;
+          highscoreObj.textContent = highscore;
+      }
     } else if (userGuess > randNum) {
        msgObj.textContent = "Too High!";
        score--;
@@ -52,11 +55,63 @@ function guess() {
     } else {
         msgObj.textContent = "Too Low!";
         score--;
-        scoreObj.textContent = score;
+        scoreObj.textContent = score; 
     }
-}
+
+    // extension
+        let diff = Math.abs(randNum - userGuess);
+        if (diff >= 19) {
+          document.body.style.backgroundColor = "#0f100f";
+      } else if (diff >= 17) {
+          document.body.style.backgroundColor = "#141814";
+      } else if (diff >= 15) {
+          document.body.style.backgroundColor = "#181f18";
+      } else if (diff >= 13) {
+          document.body.style.backgroundColor = "#1b251b";
+      } else if (diff >= 11) {
+          document.body.style.backgroundColor = "#1e2c1e";
+      } else if (diff >= 9) {
+          document.body.style.backgroundColor = "#203320";
+      } else if (diff >= 7) {
+          document.body.style.backgroundColor = "#223b22";
+      } else if (diff >= 5) {
+          document.body.style.backgroundColor = "#244424";
+      } else if (diff >= 3) {
+          document.body.style.backgroundColor = "#264e26";
+      } else if (diff >= 1) {
+          document.body.style.backgroundColor = "#285a28";
+      } else {
+          document.body.style.backgroundColor = "#39c039";
+      }
+  }
 
 document.querySelector(".check").addEventListener("click", guess);
 
 // again button
-document.querySelector(".again").addEventListener("click", 
+function again() {
+  alert ("Start Over!");
+
+  //recalculate randNum
+  randNum = getRandomIntInclusive(1, 20);
+  console.log("New Secret Number:", randNum);
+
+  //background back to black
+  document.body.style.backgroundColor = "black";
+
+  //change number box to ?
+  numberObj.textContent = "?";
+
+  //change score variable to 20, score message to score
+  score = 20;
+  scoreObj.textContent = score;
+
+  //message "start guessing"
+  msgObj.textContent = "Start guessing...";
+
+  //change guess box back to 0
+  guessObj.value = "";
+
+  checkBtn.disabled = false;
+}
+
+document.querySelector(".again").addEventListener("click", again);
